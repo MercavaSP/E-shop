30dfb1c1363716b98c783910963540094e93fd03
MercavaSP <staspip@gmail.com>
2021-03-29 19:31:46 +0300

    added slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function(){
 
     $('.hero-block__slider').slick({
+        dots: true,
+        prevArrow: '<button class="hero__btn hero__btn-prev"><img src="/assets/images/left_arrow.svg" alt="left arrow"></button>',
+        nextArrow: '<button class="hero__btn hero__btn-next"><img src="/assets/images/right_arrow.svg" alt="right arrow"></button>'
 
     });
 
