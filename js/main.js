f9b651e0c9aaaa5774acf1032a5cc8078fc749df
MercavaSP <staspip@gmail.com>
2021-04-01 15:24:21 +0300

    images folder mover to assets folder

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(function(){
 
     $('.hero-block__slider').slick({
         dots: true,
-        prevArrow: '<button class="hero__btn hero__btn-prev"><img src="/images/left_arrow.svg" alt="left arrow"></button>',
-        nextArrow: '<button class="hero__btn hero__btn-next"><img src="/images/right_arrow.svg" alt="right arrow"></button>'
+        prevArrow: '<button class="hero__btn hero__btn-prev"><img src="assets/images/left_arrow.svg" alt="left arrow"></button>',
+        nextArrow: '<button class="hero__btn hero__btn-next"><img src="assets/images/right_arrow.svg" alt="right arrow"></button>'
 
     });
 
