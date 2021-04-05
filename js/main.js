55d0760acda429ad74f56d999da2f80e719c6fc5
MercavaSP <staspip@gmail.com>
2021-04-05 12:16:43 +0300

    tabs and search done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-
+// Настраиваем слайдер для слайдера в Hero
     $('.hero-block__slider').slick({
         dots: true,
         prevArrow: '<button class="hero__btn hero__btn-prev"><img src="assets/images/left_arrow.svg" alt="left arrow"></button>',
@@ -7,4 +7,21 @@ $(function(){
 
     });
 
+// Настраиваем Табсы
+    $('.search__tabs-item').on('click', function(e){
+        
+        // Убираем активность ссылок 
+        e.preventDefault();
+        
+        // Убираем класс активности с ссылок
+        $('.search__tabs-item').removeClass('search__tabs-item--active');
+        $('.search__content-item').removeClass('search__content-item--active');
+        
+        // Добавляем класс на ссылку по клику
+        $(this).addClass('search__tabs-item--active');
+        $($(this).attr('href')).addClass('search__content-item--active');
+    
+    });
+
 });
+
